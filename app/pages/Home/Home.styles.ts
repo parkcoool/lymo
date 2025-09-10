552622eb98cf168432bde2a60a553e9ae3ce639f
parkcoool <yickle1345@naver.com>
2025-09-10 17:11:36 +0900

    Feat: Add search and music sections to Home page
    - Added SVG logo asset for branding.
    - Configured vite-plugin-svgr for SVG imports in the project.

--- app/pages/Home/Home.styles.ts
+++ app/pages/Home/Home.styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import IconWrapper from "~/components/IconWrapper";
+
 export const Container = styled.div`
   display: flex;
   width: 100%;
@@ -9,4 +11,82 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: flex-start;
   background-color: ${(props) => props.theme.colors.background};
+`;
+
+export const SearchSection = styled.section`
+  display: flex;
+  height: 400px;
+  padding: 10px 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  align-self: stretch;
+`;
+
+export const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  opacity: 0.8;
+  color: ${(props) => props.theme.colors.onBackground};
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const LogoIconWrapper = styled(IconWrapper)`
+  width: 40px;
+  height: 40px;
+  color: ${(props) => props.theme.colors.onBackground};
+`;
+
+export const SearchBox = styled.button`
+  display: flex;
+  padding: 15px 20px;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-self: stretch;
+  border-radius: 32px;
+  background: ${(props) => props.theme.colors.surface};
+  border: none;
+  cursor: pointer;
+  white-space: nowrap;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  padding: 10px 0;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+
+export const SectionTitle = styled.h2`
+  display: flex;
+  padding: 0 20px;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+  color: ${(props) => props.theme.colors.onBackground};
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  opacity: 0.8;
+`;
+
+export const SectionIconWrapper = styled(IconWrapper)`
+  width: 24px;
+  height: 24px;
+`;
+
+export const SectionContent = styled.div`
+  display: flex;
+  padding: 20px;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+  overflow-x: auto;
 `;
--- app/pages/Home/Home.tsx
+++ app/pages/Home/Home.tsx
@@ -1,5 +1,92 @@
+import { MdLyrics, MdTrendingUp } from "react-icons/md";
+
+import LogoIcon from "~/assets/logo.svg?react";
+import { CompactSong, LyricsSong } from "~/components/Song";
+
 import * as S from "./Home.styles";
 
 export default function Home() {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      {/* 검색 섹션 */}
+      <S.SearchSection>
+        <S.Brand>
+          <S.LogoIconWrapper>
+            <LogoIcon />
+          </S.LogoIconWrapper>
+          Lymo
+        </S.Brand>
+        <S.SearchBox>음악 검색</S.SearchBox>
+      </S.SearchSection>
+
+      {/* 인기 섹션 */}
+      <S.Section>
+        <S.SectionTitle>
+          <S.SectionIconWrapper>
+            <MdTrendingUp />
+          </S.SectionIconWrapper>
+          인기
+        </S.SectionTitle>
+        <S.SectionContent>
+          <CompactSong
+            id={"1"}
+            title={"song title"}
+            coverUrl={"https://placehold.co/200x200"}
+          />
+          <CompactSong
+            id={"2"}
+            title={"song title"}
+            coverUrl={"https://placehold.co/200x200"}
+          />
+          <CompactSong
+            id={"3"}
+            title={"song title"}
+            coverUrl={"https://placehold.co/200x200"}
+          />
+        </S.SectionContent>
+      </S.Section>
+
+      {/* 가사가 좋은 음악 섹션 */}
+      <S.Section>
+        <S.SectionTitle>
+          <S.SectionIconWrapper>
+            <MdLyrics />
+          </S.SectionIconWrapper>
+          가사가 좋은 음악
+        </S.SectionTitle>
+        <S.SectionContent>
+          <LyricsSong
+            id={"1"}
+            title={"song title"}
+            artist={"artist name"}
+            coverUrl={"https://placehold.co/200x200"}
+            lyrics={`When you were here before
+Couldn't look you in the eye
+You're just like an angel
+Your skin makes me cry`}
+          />
+          <LyricsSong
+            id={"2"}
+            title={"song title"}
+            artist={"artist name"}
+            coverUrl={"https://placehold.co/200x200"}
+            lyrics={`When you were here before
+Couldn't look you in the eye
+You're just like an angel
+Your skin makes me cry`}
+          />
+          <LyricsSong
+            id={"3"}
+            title={"song title"}
+            artist={"artist name"}
+            coverUrl={"https://placehold.co/200x200"}
+            lyrics={`When you were here before
+Couldn't look you in the eye
+You're just like an angel
+Your skin makes me cry`}
+          />
+        </S.SectionContent>
+      </S.Section>
+    </S.Container>
+  );
 }
