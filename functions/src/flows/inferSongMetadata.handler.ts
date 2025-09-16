b1b48b053b15f39cb0e388565c6b0fa0216e9e56
parkcoool <yickle1345@naver.com>
2025-09-16 13:44:09 +0900

    Feat: Add secret management for geminiApiKey in `inferSongMetadata` flow

--- functions/src/flows/inferSongMetadata.handler.ts
+++ functions/src/flows/inferSongMetadata.handler.ts
@@ -1,10 +1,14 @@
 import { onCallGenkit } from "firebase-functions/v2/https";
+import { defineSecret } from "firebase-functions/params";
 
 import { inferSongMetadataFlow } from "./inferSongMetadata.flow";
+
+const geminiApiKey = defineSecret("GEMINI_API_KEY");
 
 const inferSongMetadata = onCallGenkit(
   {
     authPolicy: (auth) => auth?.token?.email_verified ?? false,
+    secrets: [geminiApiKey],
     region: "asia-northeast3",
   },
   inferSongMetadataFlow
