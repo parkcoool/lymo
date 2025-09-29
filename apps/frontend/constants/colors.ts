da901ae9396d05884d99db96f586f6e208078d16
parkcoool <yickle1345@naver.com>
2025-09-29 17:14:32 +0900

    Chore: Remove unused assets and add color constant

---
+++ apps/frontend/constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#50213A",
+  background: "#3B1C12",
+  onBackground: "#E0D5D2",
+  onBackgroundSubtle: "#C0ACA5",
+  surface: "CAC2BF",
+  onSurface: "#282726",
+  onSurfaceSubtle: "#514E4C",
+  primaryBackground: "#4F332A",
+  onPrimaryBackground: "#DCD6D4",
+  onPrimaryBackgroundSubtle: "#B9ADAA",
+};
