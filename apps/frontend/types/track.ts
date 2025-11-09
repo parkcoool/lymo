a49231e419ae84376468618886f1df81e14f203b
parkcoool <yickle1345@naver.com>
2025-11-09 18:19:54 +0900

    Chore: Remove unused type file `track.ts`

--- apps/frontend/types/track.ts
+++
@@ -1,8 +0,0 @@
-import { Track, TrackDetail } from "@lymo/schemas/shared";
-
-export interface TrackStreamResult {
-  duplicate: boolean;
-  notFound: boolean;
-  trackId?: string;
-  track?: Track & TrackDetail;
-}
