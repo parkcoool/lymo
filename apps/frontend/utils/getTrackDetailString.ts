b27e006b81907bfd430b246f25efcf27c1200964
parkcoool <yickle1345@naver.com>
2025-11-09 18:20:43 +0900

    Doc(fe): Add comment for `getTrackDetailString`

--- apps/frontend/utils/getTrackDetailString.ts
+++ apps/frontend/utils/getTrackDetailString.ts
@@ -4,6 +4,11 @@ interface GetTrackDetailStringProps {
   publishedAt: string | null;
 }
 
+/**
+ * @description 트랙의 상세 정보를 문자열로 반환합니다.
+ * @param GetTrackDetailStringProps 트랙의 상세 정보
+ * @returns 트랙의 상세 정보를 결합한 문자열
+ */
 export default function getTrackDetailString({
   artist,
   album,
