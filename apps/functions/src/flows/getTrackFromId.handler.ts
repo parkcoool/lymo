fcdf9749c9aa7cb8eee65b7254b2af3763c642e9
parkcoool <yickle1345@naver.com>
2025-11-22 10:51:46 +0900

    Feat(func): Add secret bindings to handlers

--- apps/functions/src/flows/getTrackFromId.handler.ts
+++ apps/functions/src/flows/getTrackFromId.handler.ts
@@ -1,12 +1,17 @@
+import { defineSecret } from "firebase-functions/params";
 import { onCallGenkit } from "firebase-functions/v2/https";
 
 import { getTrackFromIdFlow } from "./getTrackFromId.flow";
+
+const geminiApiKey = defineSecret("GEMINI_API_KEY");
+const spotifyClientSecret = defineSecret("SPOTIFY_CLIENT_SECRET");
 
 const getTrackFromId = onCallGenkit(
   {
     cors: true,
     timeoutSeconds: 300,
     region: "asia-northeast3",
+    secrets: [geminiApiKey, spotifyClientSecret],
   },
   getTrackFromIdFlow
 );
--- apps/functions/src/flows/getTrackFromMetadata.handler.ts
+++ apps/functions/src/flows/getTrackFromMetadata.handler.ts
@@ -1,12 +1,17 @@
+import { defineSecret } from "firebase-functions/params";
 import { onCallGenkit } from "firebase-functions/v2/https";
 
 import { getTrackFromMetadataFlow } from "./getTrackFromMetadata.flow";
+
+const geminiApiKey = defineSecret("GEMINI_API_KEY");
+const spotifyClientSecret = defineSecret("SPOTIFY_CLIENT_SECRET");
 
 const getTrackFromMetadata = onCallGenkit(
   {
     cors: true,
     timeoutSeconds: 300,
     region: "asia-northeast3",
+    secrets: [geminiApiKey, spotifyClientSecret],
   },
   getTrackFromMetadataFlow
 );
