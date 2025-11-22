e128024e2e0fc7e87d252e33a08d5b0ed68f9f62
parkcoool <yickle1345@naver.com>
2025-11-22 11:14:46 +0900

    Fix(Func): Complete `getTrackFromMetadataFlow`

--- apps/functions/src/flows/getTrackFromMetadata.flow.ts
+++ apps/functions/src/flows/getTrackFromMetadata.flow.ts
@@ -1,8 +1,11 @@
+import { TrackDoc } from "@lymo/schemas/doc";
 import {
   GetTrackFromMetadataFlowInputSchema,
   GetTrackFromMetadataFlowOutputSchema,
   GetTrackFromMetadataFlowStreamSchema,
 } from "@lymo/schemas/function";
+import admin from "firebase-admin";
+import { DocumentReference } from "firebase-admin/firestore";
 import { logger } from "genkit/logging";
 
 import ai from "@/core/genkit";
@@ -35,19 +38,46 @@ export const getTrackFromMetadataFlow = ai.defineFlow(
       if (!spotifyResult) return null;
 
       // 2) getTrackFromIdFlow 호출
-      const { output, stream } = getTrackFromIdFlow.stream({
+      let { output, stream } = getTrackFromIdFlow.stream({
         trackId: spotifyResult.id,
         language: input.language,
         model: input.model,
       });
 
-      // 2-1) 스트림 처리
-      for await (const chunk of stream) {
-        sendChunk(chunk);
-      }
+      // 2-1) 스트림 처리 및 최종 결과 반환
+      for await (const chunk of stream) sendChunk(chunk);
+      let track = await output;
+      if (track) return track;
 
-      // 2-2) 최종 결과 반환
-      return await output;
+      // 3) track 문서 생성
+      const date = new Date().toISOString();
+      const trackDocRef = admin
+        .firestore()
+        .collection("tracks")
+        .doc(spotifyResult.id) as DocumentReference<TrackDoc>;
+      const trackData: TrackDoc = {
+        title: spotifyResult.title,
+        artist: spotifyResult.artist,
+        album: spotifyResult.album,
+        coverUrl: spotifyResult.coverUrl,
+        duration: spotifyResult.duration,
+        publishedAt: spotifyResult.publishedAt,
+        createdAt: date,
+        play: 0,
+      };
+      await trackDocRef.set(trackData);
+
+      // 4) 다시 getTrackFromIdFlow 호출
+      ({ output, stream } = getTrackFromIdFlow.stream({
+        trackId: spotifyResult.id,
+        language: input.language,
+        model: input.model,
+      }));
+
+      // 2-1) 스트림 처리 및 최종 결과 반환
+      for await (const chunk of stream) sendChunk(chunk);
+      track = await output;
+      return track;
     } catch (error) {
       logger.error("An error occurred in getTrackFromMetadataFlow", error);
       return null;
