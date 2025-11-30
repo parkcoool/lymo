98d30bb9f28b24b6c24ac0f4073284dc59420eb4
parkcoool <yickle1345@naver.com>
2025-12-01 01:00:20 +0900

    Feat(func): Add `databaseURL` to Firebase admin initialization for Realtime Database

--- apps/functions/src/core/admin.ts
+++ apps/functions/src/core/admin.ts
@@ -4,6 +4,7 @@ import { setGlobalOptions } from "firebase-functions";
 
 admin.initializeApp({
   credential: applicationDefault(),
+  databaseURL: "https://lymo-65c4d-default-rtdb.firebaseio.com",
 });
 
 setGlobalOptions({ maxInstances: 10, region: "asia-northeast3" });
