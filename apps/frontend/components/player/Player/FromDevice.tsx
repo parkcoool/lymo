9107e28036d25857a93d127992fed22956ee373a
parkcoool <yickle1345@naver.com>
2025-11-30 14:34:41 +0900

    Feat(fe): Refactor `FromDevice` component to utilize `useStoryQuery` for fetching story data and improve query parameters handling

--- apps/frontend/components/player/Player/FromDevice.tsx
+++ apps/frontend/components/player/Player/FromDevice.tsx
@@ -1,5 +1,8 @@
+import { useSettingStore } from "@/contexts/useSettingStore";
 import useRetrieveTrack from "@/hooks/queries/useRetrieveTrack";
+import useStoryQuery from "@/hooks/queries/useStoryQuery";
 
+import PlayerContent from "./PlayerContent";
 import StoryRequest from "./StoryRequest";
 
 interface FromDeviceProps {
@@ -9,7 +12,16 @@ interface FromDeviceProps {
 }
 
 export default function FromDevice(props: FromDeviceProps) {
+  const { setting } = useSettingStore();
+
   const { data: track } = useRetrieveTrack(props);
+  const { data: story } = useStoryQuery({
+    trackId: track?.id,
+    language: setting.defaultLanguage,
+    enabled: !!track?.id,
+  });
+
+  if (story !== null) return <PlayerContent track={track?.data} story={story} />;
 
   return <StoryRequest track={track?.data} {...props} />;
 }
