196872f04d8cdd3fcb64330da9e034decaeae068
parkcoool <yickle1345@naver.com>
2025-12-12 14:04:24 +0900

    Feat(func): Enable Firebase telemetry in genkit initialization

--- apps/functions/src/core/genkit.ts
+++ apps/functions/src/core/genkit.ts
@@ -1,6 +1,8 @@
+import { enableFirebaseTelemetry } from "@genkit-ai/firebase";
 import { googleAI } from "@genkit-ai/google-genai";
 import { genkit } from "genkit";
 
 const ai = genkit({ plugins: [googleAI()] });
+enableFirebaseTelemetry();
 
 export default ai;
