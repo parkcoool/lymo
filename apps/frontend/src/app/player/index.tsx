5e794f29caf15a1280e16e7b4982dd7faecc2f0a
parkcoool <yickle1345@naver.com>
2025-12-14 13:41:38 +0900

    Refactor(fe): Remove `react-error-boundary` dependency and update error handling in components

--- apps/frontend/src/app/player/index.tsx
+++ apps/frontend/src/app/player/index.tsx
@@ -1,5 +1,12 @@
+import { ErrorBoundaryProps } from "expo-router";
+
+import ErrorIndicator from "@/features/player/components/ErrorIndicator";
 import PlayerContent from "@/features/player/components/PlayerContent";
 
 export default function Player() {
   return <PlayerContent />;
+}
+
+export function ErrorBoundary(props: ErrorBoundaryProps) {
+  return <ErrorIndicator {...props} />;
 }
--- apps/frontend/src/features/player/components/ErrorIndicator/ErrorIndicator.tsx
+++ apps/frontend/src/features/player/components/ErrorIndicator/ErrorIndicator.tsx
@@ -1,28 +1,19 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import type { FallbackProps } from "react-error-boundary";
+import type { ErrorBoundaryProps } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./ErrorIndicator.styles";
 
-export default function ErrorIndicator({
-  error,
-  resetErrorBoundary,
-}: FallbackProps) {
+export default function ErrorIndicator({ error, retry }: ErrorBoundaryProps) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <MaterialIcons name="error" size={60} style={styles.errorIcon} />
-        <Text style={styles.errorMessage}>
-          {error.message ?? "오류가 발생했어요."}
-        </Text>
+        <Text style={styles.errorMessage}>{error.message ?? "오류가 발생했어요."}</Text>
       </View>
 
-      <TouchableOpacity style={styles.retryButton} onPress={resetErrorBoundary}>
-        <MaterialIcons
-          name="refresh"
-          size={20}
-          style={styles.retryButtonIcon}
-        />
+      <TouchableOpacity style={styles.retryButton} onPress={retry}>
+        <MaterialIcons name="refresh" size={20} style={styles.retryButtonIcon} />
         <Text style={styles.retryButtonContent}>다시 시도</Text>
       </TouchableOpacity>
     </View>
