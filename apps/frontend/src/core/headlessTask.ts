220fbb1f3b48ac016b39cead995eaea37765bbcc
parkcoool <yickle1345@naver.com>
2025-12-28 20:25:43 +0900

    Feat(fe): Enhance `LymoMediaTask` to include notification frequency settings

--- apps/frontend/src/core/headlessTask.ts
+++ apps/frontend/src/core/headlessTask.ts
@@ -4,6 +4,7 @@ import { AppRegistry, LogBox } from "react-native";
 
 import { MediaModule } from "@/core/mediaModule";
 import retrieveTrackNoti from "@/entities/noti/api/retrieveTrackNoti";
+import { Setting } from "@/entities/setting/models/types";
 import isSettingJSON from "@/entities/setting/utils/isSettingJSON";
 import parseSettingJSON from "@/entities/setting/utils/parseSettingJSON";
 
@@ -12,9 +13,17 @@ LogBox.ignoreLogs([
   "registerHeadlessTask or registerCancellableHeadlessTask called multiple times",
 ]);
 
+const FREQUENCY_THRESHOLDS = {
+  always: 0,
+  normal: 1000 * 60 * 60, // 1시간
+  minimal: 1000 * 60 * 60 * 3, // 3시간
+};
+
 const LymoMediaTask = async (data: { title: string; artist: string; duration: number }) => {
   // 1) 설정 불러오기
   let language: Language;
+  let notificationFrequency: Setting["notificationFrequency"];
+
   const settingString = await AsyncStorage.getItem("setting");
 
   // 2) 언어 설정 파싱
@@ -26,14 +35,27 @@ const LymoMediaTask = async (data: { title: string; artist: string; duration: nu
       if (!isSettingJSON(settingJSON)) return;
       const parsed = parseSettingJSON(settingJSON);
       language = parsed.language;
+      notificationFrequency = parsed.notificationFrequency;
     } catch {
       language = "ko";
     }
   }
 
+  if (!notificationFrequency || notificationFrequency === "never") return;
+
+  // 3) 가장 최근 알림 전송 시각 가져오기
+  let lastTimestamp = 0;
+  try {
+    lastTimestamp = parseInt((await AsyncStorage.getItem("lastInsightTimestamp")) ?? "0");
+  } catch {}
+
+  const nowTimestamp = Date.now();
+  const delta = nowTimestamp - lastTimestamp;
+  if (delta < FREQUENCY_THRESHOLDS[notificationFrequency]) return;
+
+  // 4) 메시지 가져오기
   let message: string;
   try {
-    // 3) 메시지 가져오기
     const { data: response } = await retrieveTrackNoti({
       ...data,
       durationInSeconds: data.duration,
@@ -46,12 +68,17 @@ const LymoMediaTask = async (data: { title: string; artist: string; duration: nu
     return;
   }
 
-  // 4) 현재 재생 중인 트랙이 아닐 경우 알림 표시 안 함
+  // 5) 현재 재생 중인 트랙이 아닐 경우 알림 표시 안 함
   const deviceMedia = await MediaModule.getCurrentMediaState();
   if (!deviceMedia || deviceMedia.title !== data.title) return;
 
+  // 6) 최근 알림 전송 시각 저장
   try {
-    // 5) 알림 표시
+    await AsyncStorage.setItem("lastInsightTimestamp", nowTimestamp.toString());
+  } catch {}
+
+  try {
+    // 7) 알림 표시
     MediaModule.showInsightNotification(`${data.title}`, message);
   } catch (error) {
     console.error("[HeadlessJS] Error:", error);
