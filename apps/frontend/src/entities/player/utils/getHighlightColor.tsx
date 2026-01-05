ce10673704c1a2ead10a29849c3f1e4a30157864
parkcoool <yickle1345@naver.com>
2026-01-05 14:35:56 +0900

    Feat(fe): 단어 해석 하이라이트 색상 개선

--- apps/frontend/src/entities/player/utils/getHighlightColor.tsx
+++ apps/frontend/src/entities/player/utils/getHighlightColor.tsx
@@ -1,13 +1,5 @@
 export default function getHighlightColor(index: number) {
-  const highlightColors = [
-    "#FF0000",
-    "#FFA500",
-    "#FFFF00",
-    "#008000",
-    "#0000FF",
-    "#4B0082",
-    "#EE82EE",
-  ];
+  const highlightColors = ["#FF6B6B", "#FF9F43", "#FECA57", "#1DD1A1", "#54A0FF", "#FF9FF3"];
 
   return highlightColors[index % highlightColors.length];
 }
