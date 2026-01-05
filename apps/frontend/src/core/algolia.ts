1931e5359515ba88533687704265690a89eca04d
parkcoool <yickle1345@naver.com>
2026-01-05 17:20:55 +0900

    Feat(schemas): `StorySchema`에서 `GeneratedStoryFields`를 partial로 변경

--- apps/frontend/src/core/algolia.ts
+++
@@ -1,6 +0,0 @@
-import { searchClient } from "@algolia/client-search";
-
-export const algoliaClient = searchClient(
-  process.env.EXPO_PUBLIC_ALGOLIA_ID!,
-  process.env.EXPO_PUBLIC_ALGOLIA_API_KEY!
-);
