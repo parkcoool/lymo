fb8074ebab8da125a01bebf6e1bb9b0e2eb91ff2
parkcoool <yickle1345@naver.com>
2026-01-10 23:39:57 +0900

    Feat(fe): 알림 권한이 부여되면 `notificationFrequency` 설정을 `normal`로 설정하도록 수정

--- apps/frontend/src/entities/home/ui/InsightBottomSheet/InsightBottomSheet.tsx
+++ apps/frontend/src/entities/home/ui/InsightBottomSheet/InsightBottomSheet.tsx
@@ -21,18 +21,17 @@ export default function InsightBottomSheet() {
   const ref = useRef<BottomSheetModal>(null);
 
   useEffect(() => {
-    let timeout: number | undefined;
-    if (!granted)
-      timeout = setTimeout(() => {
+    // 권한이 부여되지 않았으면 모달 표시
+    if (!granted) {
+      const timeout = setTimeout(() => {
         ref.current?.present();
       }, 1000);
 
-    return () => {
-      if (timeout) clearTimeout(timeout);
-    };
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
   }, [granted]);
-
-  if (granted) return null;
 
   // 권한 부여 버튼 핸들러
   const handleGrant = () => {
@@ -42,7 +41,12 @@ export default function InsightBottomSheet() {
   // 모달 닫기 핸들러
   const handleDismiss = () => {
     // notificationFrequency가 설정되지 않았다면 'never'로 설정
-    updateSetting((prev) => ({ notificationFrequency: "never", ...prev }));
+    updateSetting((prev) => {
+      if (prev.notificationFrequency === undefined)
+        return { ...prev, notificationFrequency: "never" };
+
+      return prev;
+    });
   };
 
   return (
--- apps/frontend/src/features/home/components/HomeOverlay.tsx
+++ apps/frontend/src/features/home/components/HomeOverlay.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import useCheckNotificationListenerPermission from "@/entities/deviceMedia/hooks/useCheckNotificationListenerPermission";
 import useCheckNotificationPermission from "@/entities/deviceMedia/hooks/useCheckNotificationPermission";
 import InsightBottomSheet from "@/entities/home/ui/InsightBottomSheet";
@@ -5,8 +7,22 @@ import { useSettingStore } from "@/entities/setting/models/settingStore";
 
 export default function HomeOverlay() {
   const { setting } = useSettingStore();
+  const { updateSetting } = useSettingStore();
+
   const notificationListenerGranted = useCheckNotificationListenerPermission();
   const notificationGranted = useCheckNotificationPermission();
+
+  useEffect(() => {
+    if (!notificationGranted) return;
+
+    // notificationFrequency가 설정되지 않았다면 'normal'로 설정
+    updateSetting((prev) => {
+      if (prev.notificationFrequency === undefined)
+        return { ...prev, notificationFrequency: "normal" };
+
+      return prev;
+    });
+  }, [notificationGranted, updateSetting]);
 
   if (!notificationListenerGranted) return null;
   if (notificationGranted) return null;
